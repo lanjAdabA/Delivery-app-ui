079bd59a730096dc57769d063e2eaeb8ba78906d
jaeC <thingbaijamjessikhuman@gmail.com>
2022-06-06 20:30:58 +0530

    updated home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,40 +10,84 @@ class UserHome extends StatefulWidget {
 }
 
 class _UserHomeState extends State<UserHome> {
+  List<Map<String, dynamic>> data = [
+    {
+      "description": "apple",
+      "quantity": 1,
+      "mrp": 280,
+      "img":
+          "https://st.depositphotos.com/1020804/2370/i/600/depositphotos_23706663-stock-photo-red-apple-with-leaf-and.jpg",
+      "offerprice": 230,
+      "deliveryType": "cod",
+      "expectedDelivery": "23rd june 2022"
+    },
+    {
+      "description": "green apple",
+      "quantity": 3,
+      "mrp": 260,
+      "img":
+          "https://media.istockphoto.com/photos/green-apple-with-leaf-isolated-on-white-clipping-path-included-picture-id629734762?b=1&k=20&m=629734762&s=170667a&w=0&h=-JTO_KaiODRi-5i8p54XBAxqMzfijkrUPaIjxBtRRfA=",
+      "offerprice": 240,
+      "deliveryType": "cod",
+      "expectedDelivery": "23rd june 2022"
+    },
+    {
+      "description": "banana",
+      "quantity": 1,
+      "mrp": 80,
+      "img": "https://www.wiffens.com/content/uploads/bananas-cavendish-1.jpeg",
+      "offerprice": 50,
+      "deliveryType": "cod",
+      "expectedDelivery": "23rd june 2022"
+    },
+    {
+      "description": "pine apple",
+      "quantity": 4,
+      "mrp": 80,
+      "img": "https://m.media-amazon.com/images/I/71bNsWlkyeL._SL1500_.jpg",
+      "offerprice": 60,
+      "deliveryType": "cod",
+      "expectedDelivery": "23rd june 2022"
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Deals of the Week"),
-      ),
-      body: SingleChildScrollView(
-        child: GridView.count(
-          mainAxisSpacing: 23,
-          crossAxisSpacing: 23,
-          padding: const EdgeInsets.all(20),
-          shrinkWrap: true,
-          crossAxisCount: 2,
-          children: [
-            Container(
-              height: 1000,
-              color: Colors.red,
-            ),
-            Container(
-              height: 1000,
-              color: Colors.blue,
-            ),
-            Container(
-              height: 1000,
-              color: Colors.green,
-            ),
-            Container(
-              height: 1000,
-              color: Colors.purple,
-            ),
-            //MyCard(),
-          ],
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Deals of the Week"),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: GridView.builder(
+              padding: const EdgeInsets.all(10),
+              shrinkWrap: true,
+              itemCount: data.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  //mainAxisExtent: 400,
+                  crossAxisCount: 2,
+                  childAspectRatio: 3 / 4),
+              itemBuilder: (c, i) {
+                return MyCard(
+                  description: data[i]["description"],
+                  deliveryType: data[i]["deliveryType"],
+                  expectedDelivery: data[i]["expectedDelivery"],
+                  quantity: data[i]["quantity"],
+                  mrp: data[i]["mrp"],
+                  offerprice: data[i]["offerprice"],
+                  img: data[i]["img"],
+                );
+              }),
+
+          /*   MyCard(
+              description: "tom",
+              deliveryType: "deliveryType",
+              expectedDelivery: "expectedDelivery",
+              quantity: 23,
+              mrp: 23,
+              offerprice: 23)), */
+        ));
   }
 }
--- lib/widgets/mycard.dart
+++ lib/widgets/mycard.dart
@@ -1,7 +1,25 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 class MyCard extends StatefulWidget {
-  const MyCard({Key? key}) : super(key: key);
+  final String description;
+  final int quantity;
+  final int mrp;
+  final String img;
+  final int offerprice;
+  final String deliveryType;
+  final String expectedDelivery;
+
+  const MyCard(
+      {Key? key,
+      required this.description,
+      required this.deliveryType,
+      required this.expectedDelivery,
+      required this.quantity,
+      required this.mrp,
+      required this.offerprice,
+      required this.img})
+      : super(key: key);
 
   @override
   State<MyCard> createState() => _MyCardState();
@@ -11,56 +29,71 @@ class _MyCardState extends State<MyCard> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          //stack
-          Stack(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Icon(
-                    Icons.check_box_outline_blank_rounded,
-                    color: Colors.red,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            //stack
+            Stack(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Icon(
+                      Icons.check_box_outline_blank_rounded,
+                      color: Colors.green,
+                    ),
+                    Text(widget.quantity.toString())
+                  ],
+                ),
+                Center(
+                  child: SizedBox(
+                    height: 130,
+                    width: 130,
+                    child: Image.network(widget.img),
                   ),
-                  Text("quantity"),
-                ],
-              ),
-              Center(
-                child: SizedBox(
-                  height: 90,
-                  width: 90,
-                  child: Image.network(
-                      "https://3.imimg.com/data3/KQ/WH/MY-16577405/apple-amari-250x250.jpg"),
                 ),
-              ),
-            ],
-          ),
-          const Text("description"),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [Text("mrp"), Text("offer_price")],
-          ),
-          SizedBox(
-            height: 30,
-            width: 120,
-            child: ElevatedButton(
-                style: ButtonStyle(
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16)))),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [Icon(Icons.add), Text("Add Item")],
+              ],
+            ),
+            Text(
+              widget.description,
+              maxLines: 2,
+            ),
+
+            // Text(item_name),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  " ${widget.mrp}   RS.",
+                  style: const TextStyle(
+                      color: Colors.grey,
+                      decoration: TextDecoration.lineThrough),
                 ),
-                onPressed: () {
-                  setState(() {});
-                }),
-          ),
-          const Text("type of delivery"),
-          const Text("(expect delivery by)"),
-        ],
+                Text(" " + widget.offerprice.toString() + "  RS")
+              ],
+            ),
+            SizedBox(
+              height: 30,
+              width: 120,
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16)))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [Icon(Icons.add), Text("Add Item")],
+                  ),
+                  onPressed: () {
+                    setState(() {});
+                  }),
+            ),
+            Text(widget.deliveryType),
+            Text(widget.expectedDelivery.toString()),
+          ],
+        ),
       ),
     );
   }
