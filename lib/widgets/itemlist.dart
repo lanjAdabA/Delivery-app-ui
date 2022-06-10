0bd742d9f6c0fadb345d87a65fca64212492ce71
jaeC <thingbaijamjessikhuman@gmail.com>
2022-06-10 12:29:03 +0530

    last commit

--- lib/widgets/itemlist.dart
+++ lib/widgets/itemlist.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'mycard.dart';
 
 class ItemList extends StatefulWidget {
   const ItemList({Key? key}) : super(key: key);
@@ -12,19 +13,91 @@ class _ItemListState extends State<ItemList> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.grey[300],
-      padding: const EdgeInsets.all(8),
+      padding: const EdgeInsets.all(5),
       child: Row(
         children: [
+          const Text(
+            " 1.   ",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
           Column(
-            children: [
-              SizedBox(
-                height: 100,
-                child: Image.network(
+            children: const [
+              CircleAvatar(
+                radius: 40,
+                backgroundImage: NetworkImage(
                     "https://m.media-amazon.com/images/I/71bNsWlkyeL._SL1500_.jpg"),
               ),
-              const Text("description")
+              Text("description")
             ],
           ),
+          const SizedBox(
+            width: 20,
+          ),
+          const SizedBox(
+              width: 140,
+              child: Text(
+                  "more descriptions. more descriptions. more descriptions.")),
+          SizedBox(
+            height: 30,
+            width: 120,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      // fixedSize: ,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5)),
+                      minimumSize: const Size(40, 30),
+                      padding: const EdgeInsets.only(left: 0.5),
+                    ),
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.remove,
+                    )),
+                const Text("01"),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      // fixedSize: ,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5)),
+                      minimumSize: const Size(40, 30),
+                      padding: const EdgeInsets.only(left: 0.5),
+                    ),
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.add,
+                    )),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 80,
+            width: 80,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: (const Color(0xFFFF9000)),
+                // fixedSize: ,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40)),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(
+                    Icons.delete,
+                    size: 40,
+                  ),
+                  Text(
+                    "delete from cart",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: (10), color: Colors.grey),
+                  )
+                ],
+              ),
+              onPressed: () {},
+            ),
+          )
         ],
       ),
     );
--- lib/widgets/mycard.dart
+++ lib/widgets/mycard.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class MyCard extends StatefulWidget {
@@ -28,6 +30,9 @@ class MyCard extends StatefulWidget {
 }
 
 class _MyCardState extends State<MyCard> {
+  String displayText = '+ Add';
+  var inCart = true;
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -100,18 +105,59 @@ class _MyCardState extends State<MyCard> {
             ),
             SizedBox(
               height: 30,
-              width: 120,
-              child: ElevatedButton(
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16)))),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [Icon(Icons.add), Text("Add Item")],
-                  ),
-                  onPressed: () {
-                    setState(() {});
-                  }),
+              width: 130,
+              child: inCart
+                  ? ElevatedButton(
+                      style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16)))),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // const Icon(
+                          //   Icons.add,
+                          //   size: 12,
+                          // ),
+                          Text(displayText)
+                        ],
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          displayText = 'Added to cart';
+
+                          inCart = !inCart;
+
+                          log(inCart ? 'is in cart' : 'is not in cart');
+                        });
+                      })
+                  : ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.grey),
+                          shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16)))),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // const Icon(
+                          //   Icons.add,
+                          //   size: 12,
+                          // ),
+                          Text(displayText)
+                        ],
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          displayText = 'Added to cart';
+
+                          inCart = !inCart;
+                          // log(inCart ? (y ? 'll': 'kk') : 'is not in cart');
+
+                          log(inCart ? 'is in cart' : 'is not in cart');
+                        });
+                      }),
             ),
             Text(
               widget.deliveryType,
